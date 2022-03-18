f6c5a62a721079866a12339f6faf04af1312737b
Moni73 <100587808+Moni73@users.noreply.github.com>
2022-03-18 16:53:19 +0100

    Finish2

--- card.js
+++ card.js
@@ -1,22 +1,22 @@
-let question = document.getElementById("question");
+const question = document.getElementById("question");
 question.addEventListener("input", () => {
-  let count = question.value.length;
+  const count = question.value.length;
   document.getElementById(
     "count-result"
   ).textContent = `Number of letters: ${count}`;
 });
 
-let question1 = document.getElementById("question1");
+const question1 = document.getElementById("question1");
 question1.addEventListener("input", () => {
-  let count = question1.value.length;
+  const count = question1.value.length;
   document.getElementById(
     "count-result1"
   ).textContent = `Number of letters: ${count}`;
 });
 
-let question2 = document.getElementById("question2");
+const question2 = document.getElementById("question2");
 question2.addEventListener("input", () => {
-  let count = question2.value.length;
+  const count = question2.value.length;
   document.getElementById(
     "count-result2"
   ).textContent = `Number of letters: ${count}`;
